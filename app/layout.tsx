02bd2e3f9f937857fea358cabc989a0cd029bc5d
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2025-05-21 14:41:45 +0530

    update date on hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${poppins.variable} font-sans`}>{children}
-        <SpeedInsights />
         <Analytics />
+        <SpeedInsights />
       </body>
       
     </html>
