ac6b89d23afab8b671fb02099bef76f3156d086a
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2025-07-23 12:24:28 +0530

    updates

--- components/sections/Sponsors.tsx
+++ components/sections/Sponsors.tsx
@@ -180,9 +180,9 @@ export default function Sponsors() {
             cat === "Industry Sponsors" || cat === "Digital & Media Partners";
           const gridCols =
             cat === "Digital & Media Partners"
-              ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-3"
+              ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-1"
               : cat === "Industry Sponsors"
-              ? "grid-cols-1 sm:grid-cols-2"
+              ? "grid-cols-3 sm:grid-cols-4"
               : "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4";
 
           return (
